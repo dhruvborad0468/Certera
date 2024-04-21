ed7ee6d37cad4c97a2fb10303040609a9141b72e
Dhruv Borad <97727648+dhruvborad0468@users.noreply.github.com>
2024-04-22 00:46:58 +0530

    Project upload

---
+++ certera/js/custom.js
@@ -0,0 +1,51 @@
+(function ($) {
+  "use strict";
+
+  // Fixed Navbar
+  $(window).scroll(function () {
+    if ($(window).width() < 992) {
+      if ($(this).scrollTop() > 45) {
+        $(".fixed-top").addClass("bg-white shadow");
+      } else {
+        $(".fixed-top").removeClass("bg-white shadow");
+      }
+    } else {
+      if ($(this).scrollTop() > 45) {
+        $(".fixed-top").addClass("bg-white shadow").css("top", -45);
+      } else {
+        $(".fixed-top").removeClass("bg-white shadow").css("top", 0);
+      }
+    }
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  //   product slider
+  $(".owl-carousel").owlCarousel({
+    loop: false,
+    margin: 12,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      767: {
+        items: 2,
+      },
+      1199: {
+        items: 3,
+      },
+    },
+  });
+})(jQuery);
